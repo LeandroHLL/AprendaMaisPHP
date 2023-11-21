fd772426f6e8452e3cf3ebb5a98f5951ae41fe91
LeandroHll <leandrohll@live.com>
2023-11-21 01:01:17 -0300

    Titulos das páginas

--- AprendaMaisPHPv7/Template/turma.php
+++ AprendaMaisPHPv7/Template/turma.php
@@ -20,7 +20,7 @@ $objProfessores = new Professor();
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-  <title>Aprenda Mais - Ferramenta de Análise de Dados</title>
+  <title>Aprenda Mais - Turmas</title>
 </head>
 
 <body>
