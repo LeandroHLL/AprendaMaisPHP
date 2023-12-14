c286d2dff051381754dcf66567a167c040acf646
LeandroHll <leandrohll@live.com>
2023-12-14 10:29:56 -0300

    Favicon

--- Browser/Template/aluno.php
+++ Browser/Template/aluno.php
@@ -39,6 +39,7 @@ $alunos = $stmtAlunos->fetchAll(PDO::FETCH_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aprenda Mais - Alunos</title>
+    <link rel="icon" type="image/x-icon" href="./img/Aprenda-Mais-logo.ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
--- Browser/Template/previsao.php
+++ Browser/Template/previsao.php
@@ -33,6 +33,7 @@ if ($selectedTurma) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Aprenda Mais - Previsões</title>
+  <link rel="icon" type="image/x-icon" href="./img/Aprenda-Mais-logo.ico">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
